4a4a2a5244b361e1a75b8b38c84e04f47e24e63e
Tim Duckett <tim@duckett.de>
2020-06-11 18:52:11 +0200

    initial commit

---
+++ my-app.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region                    = "eu-central-1"
+  shared_credentials_file   = "/Users/tim/.aws/credentials"
+  profile                   = "default"  
+}
+
+variable "bucket_name" {
+  default = "my-app-test-bucket-duckett-de"
+}
+
+resource "aws_s3_bucket" "react_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "bucket_policy_site",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "bucket_policy_site_main",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+output "website_domain" {
+  value = aws_s3_bucket.react_bucket.website_domain
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket.react_bucket.website_endpoint
+}
